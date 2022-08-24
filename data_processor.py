0a49e91f40d44f792b46f562c2ddb562f45ec2be
sijinwnag <sijin.wang@student.unsw.edu.au>
2022-08-24 14:36:25 +1000

    s

---
+++ data_processor.py
@@ -0,0 +1,4 @@
+import pyodbc
+from sqlalchemy import create_engine
+
+engine = create_engine(r"C:\Users\sijin wang\Desktop\Thesis\RA\Module_data_project\data\raw_data\2022-05-31_2022-07-01.accdb")
--- data_processor_object.py
+++ data_processor_object.py
@@ -2,6 +2,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import pyodbc
+from sqlalchemy import create_engine
 # %%-
 
 
@@ -11,9 +13,24 @@ class module_data_processor:
     """
 
 
-    def __init__(self, path, starting_date, ending_date):
+    def __init__(self, pathlist, starting_time, ending_time):
         """
-        1. Read the dataset within the given period.
+        1. Write the path, starting date and ending date into the object.
+        2. Input:
+            pathlist: a list of databse access path we want to load.
+            starting date: the first day to start.
+            ending date: the end of the time to look at.
         """
 
-        # read the access file with the given path
+        self.pathlist = pathlist
+        self.starting_time = starting_time
+        self.ending_time = ending_time
+
+
+    def dataloader(self):
+        """
+        What this function does:
+        1. Read the path list we want to get the date from.
+        2. Read the starting and ending time we are interested in.
+        3. Load the corresponding database and put it into a pd.dataframe
+        """
